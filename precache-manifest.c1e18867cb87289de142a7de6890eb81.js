a711f7b779cbab81c3190411c69f047616a92246
anton <antonirklii@gmail.com>
2021-07-23 13:55:42 +0300

    Updates

---
+++ precache-manifest.c1e18867cb87289de142a7de6890eb81.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7cd13b4ebfd9d3e423528d9afcd168f2",
+    "url": "/RestoMyWork/index.html"
+  },
+  {
+    "revision": "5376662f20bcc642f4c5",
+    "url": "/RestoMyWork/static/css/main.64f9a69d.chunk.css"
+  },
+  {
+    "revision": "a271d28567bacbfa3d6b",
+    "url": "/RestoMyWork/static/js/2.4d2f8cbb.chunk.js"
+  },
+  {
+    "revision": "5376662f20bcc642f4c5",
+    "url": "/RestoMyWork/static/js/main.f4bcccb7.chunk.js"
+  },
+  {
+    "revision": "c97ec765f236970121ef",
+    "url": "/RestoMyWork/static/js/runtime~main.3a31382f.js"
+  },
+  {
+    "revision": "698e5e807daf0026f6ac590ce288822c",
+    "url": "/RestoMyWork/static/media/food-bg.698e5e80.jpg"
+  },
+  {
+    "revision": "701d6d1d57f5d528c4db1e887ff29e82",
+    "url": "/RestoMyWork/static/media/shopping-cart-solid.701d6d1d.svg"
+  }
+]);
